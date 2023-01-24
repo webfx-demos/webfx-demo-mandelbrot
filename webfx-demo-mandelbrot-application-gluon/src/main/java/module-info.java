3d66028ed1221901c33115e02c3b2bb7ae1159b1
Bruno Salmon <dev.salmonb@gmail.com>
2023-01-24 10:36:09 +0000

    Ran 'webfx update' to consider webfx-platform-os-gluon instead of -java

--- webfx-demo-mandelbrot-application-gluon/src/main/java/module-info.java
+++ webfx-demo-mandelbrot-application-gluon/src/main/java/module-info.java
@@ -9,7 +9,7 @@ module webfx.demo.mandelbrot.application.gluon {
     requires webfx.platform.boot.java;
     requires webfx.platform.console.java;
     requires webfx.platform.json.java;
-    requires webfx.platform.os.java;
+    requires webfx.platform.os.gluon;
     requires webfx.platform.resource.gluon;
     requires webfx.platform.scheduler.java;
     requires webfx.platform.shutdown.gluon;
